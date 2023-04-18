3fdb75850728b90776b247fd6fb122f28c279578
Edward Zota <zotaedward@gmail.com>
2023-04-18 16:43:45 +0200

    esercizio base + bonus completati

--- index.php
+++ index.php
@@ -53,11 +53,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body class="text-center">
-    <div>
-        <?php
-            var_dump($arrayParkingSi);
-        ?>
-    </div>
     <form action="index.php" method="GET" class="d-flex justify-content-center py-3">
         <div class="form-group mx-5">
             <label for="parking">Parcheggio:</label>
