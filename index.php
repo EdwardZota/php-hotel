6da1d472f8c5034aa2aa61ea20c9a1ca0ff2ef8c
Edward Zota <zotaedward@gmail.com>
2023-04-18 14:08:47 +0200

    Esercizio Base Completato

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hotels & Co.</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-</head>
-<body>
     <?php
 
         $hotels = [
@@ -50,18 +40,52 @@
 
     ];
 
-    foreach($hotels as $hotel){
-        foreach($hotel as $key => $value){
-            echo $key.':'. $value . '<br/>';
-        }
-        echo '<hr/>';
-    }
-
-
-
-
     ?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotels & Co.</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <?php 
+                    foreach($hotels[0] as $key => $value ){
+                        echo "<th scope='col'>". $key ."</th>";
+                    }
+                ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+                foreach($hotels as $hotel){
+
+                    echo '<tr>';
+                    foreach($hotel as $info => $value){
+
+                        if($info == 'parking' ){
+
+                            if($value==true){
+                                $value='Si';
+                            }else{
+                                $value='No';
+                            }
+                        }
+                        echo "<td>". $value ."</td>";
+                    }
+                    echo '</tr>';
+                }
+            ?>
+        </tbody>
+    </table>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
